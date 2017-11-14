61c34094833da1a03fe005eb74eaef9e98a1591c
Joshua Ticzon <ticzon.joshua@gmail.com>
2017-11-14 00:24:35 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,12 +2,10 @@
 def speak_to_grandma (speak)
   if speak == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
-  elsif speak == "Hi Nana, how are you?"
-    puts "HUH?! SPEAK UP, SONNY"
-  elsif speak == "Hi!"
-    puts "HUH?! SPEAK UP, SONNY!"
-  elsif speak == "WHAT DID YOU EAT TODAY?" || "WHAT?"
+  elsif speak == speak.upcase
     puts "NO, NOT SINCE 1938!"
+  else
+  puts "HUH?! SPEAK UP, SONNY"
   end
 end
 # Whatever you say to grandma, she should respond with
